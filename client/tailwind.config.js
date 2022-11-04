09a91f4f102ba526cd8a1c3b79010bc8965c9c5f
name <100819836+userMauro@users.noreply.github.com>
2022-11-04 15:22:29 -0300

    add tailwind to project

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.js", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
